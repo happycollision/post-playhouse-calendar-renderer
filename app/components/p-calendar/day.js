ecbdeabe1723216f993aa0048c05b8efcedfeb9f
Don Denton <don@happycollision.com>
2018-08-08 11:40:53 -0600

    add some inputs to day, and computed
    * monthName
    * mDay
    
    Computed: firstOfMonthIdentifier

--- app/components/p-calendar/day.js
+++ app/components/p-calendar/day.js
@@ -1,5 +1,14 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName: '',
+  
+  firstOfMonthIdentifier: computed('monthName', 'mDay', function() {
+    const {monthName, mDay} = this.getProperties('monthName', 'mDay');
+    if (mDay === 1 || mDay === '1') {
+      return monthName.toLowerCase()
+    }
+    return '';
+  }),
 });
