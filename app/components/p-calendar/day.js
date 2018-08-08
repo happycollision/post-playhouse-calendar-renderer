c56743fd47cdbab15c9709314cbbc09d458acbb4
Don Denton <don@happycollision.com>
2018-08-08 17:35:36 -0600

    add 'dark' to days

--- app/components/p-calendar/day.js
+++ app/components/p-calendar/day.js
@@ -3,12 +3,17 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName: '',
-  
+
   firstOfMonthIdentifier: computed('monthName', 'mDay', function() {
     const {monthName, mDay} = this.getProperties('monthName', 'mDay');
     if (mDay === 1 || mDay === '1') {
       return monthName.toLowerCase()
     }
     return '';
+  }),
+
+  isDark: computed('shows', function() {
+    const length = this.get('shows.length');
+    return (length === 0 || length === undefined)
   }),
 });
