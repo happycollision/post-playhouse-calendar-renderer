4969c5e201b6c5710b78d58daffaa37f84963f22
Don Denton <don@happycollision.com>
2018-08-08 23:42:39 -0600

    push data into component

--- app/components/p-calendar.js
+++ app/components/p-calendar.js
@@ -1,4 +1,21 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  showData: computed(function() {
+    return [
+      {
+        startingDate: '2018-06-03',
+        showsByDay: [
+          [{title: 'Mermaid', time: '2p', id: 1}],
+          [],
+          [{title: 'Mermaid', time: '8p', id: 1}],
+          [],
+          [],
+          [{title: 'Footloose', time: '8p', id: 2}],
+          [{title: 'Footloose', time: '8p', id: 2}],
+        ]
+      }
+    ]
+  })
 });
