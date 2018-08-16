5b3e6b43b23b752f4f254aa861e6a40e91d4bb2b
Don Denton <don@happycollision.com>
2018-08-16 02:05:23 -0500

    Move functions to util file and test like mad

---
+++ app/utils/showings-data-converters.ts
@@ -0,0 +1,285 @@
+import { DateTime } from 'luxon';
+
+export const DEFAULT_TITLES = 'Mermaid,Footloose,Chitty,Urinetown,42nd St';
+export const DEFAULT_LONG_TITLES = 'Disney\'s The Little Mermaid,Footloose,Chitty Chitty Bang Bang,Urinetown,42nd Street';
+export const DEFAULT_DATES = '2018-06-01' +
+  '[1]a3b3c2e3l3t3v3x20d3n3r3u1v2y2B2E30d1h2k1l2p3r2' +
+  '[2]h3i3j2m3s3w3A30g2l3q3t3z3C20b3d3j3o2' +
+  '[3]o3p3q2u3z30c3k2m2o2u2x3B10a3d2g3i3k3q3s2' +
+  '[4]C3D30a2k3m3s3A30c3h3k2n3r3' +
+  '[5]0f3g3h2j3n2r2u3y3B30a2e2o3';
+
+export interface IIdLookup {
+  [x: number]: any;
+}
+
+export interface IDayShowings {
+  m?: number;
+  a?: number;
+  e?: number;
+}
+
+export interface IShorthandObject {
+  startingDate: string;
+  showData: IDayShowings[];
+}
+
+type TShowingCode = 'm' | 'a' | 'e' | 'ma' | 'me' | 'ae' | 'mae';
+
+export class ShowData {
+  title: string;
+  constructor(public id: number, public time: string, idLookup: IIdLookup = {}) {
+    this.title = idLookup[id] || 'NO TITLE';
+  }
+}
+
+const MATCH_SHOWING_CODE = /\d{1,2}|[mae]{1,3}/;
+const SPLIT_SHOWING_CODE = /(\d{1,2}|[mae]{1,3})/g;
+const MONTHS = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+
+export function getPaddingFor(startingDate: string): number {
+  const dateTime = DateTime.fromISO(startingDate);
+  return dateTime.weekday === 7 ? 0 : dateTime.weekday;
+}
+
+function getDaysFromDateId(dateId: string) {
+  return 'abcdefghijklmnopqrstuvwxyzABCDE'.indexOf(dateId)
+}
+
+function getDateIdFromDay(day: string | number) {
+  day = typeof day === 'string' ? parseInt(day, 10) : day;
+  return 'abcdefghijklmnopqrstuvwxyzABCDE'[day - 1]
+}
+
+function notEnoughMonths(value: string, monthsFromStart: number) { 
+  if (value === undefined) throw new Error('no value given for comparing to months'); 
+  return (value.match(/0/g) || []).length < monthsFromStart; 
+} 
+
+export function shorthandToUrl(shorthandObj: IShorthandObject) { 
+  const {startingDate, showData} = shorthandObj; 
+  const startingDay = DateTime.fromISO(startingDate).startOf('day'); 
+  const dataString = showData.reduce((acc: string[],cur,i) => { 
+    const today = startingDay.plus({days: i})
+    const yearsFromStart = today.year - startingDay.year;
+    const monthsFromStart = Math.abs(today.month - startingDay.month) + yearsFromStart * 12;
+    const {m,a,e} = cur; 
+    const showsToday = []; 
+    if (m) showsToday[m] = 'm'; 
+    if (a) showsToday[a] = (showsToday[a] || '') + 'a'; 
+    if (e) showsToday[e] = (showsToday[e] || '') + 'e'; 
+    showsToday.forEach((showingGroup: TShowingCode, i) => { 
+      if (!acc[i]) acc[i] = ''; 
+      while(notEnoughMonths(acc[i], monthsFromStart)) { acc[i] += '0'; } 
+      acc[i] += getDateIdFromDay(today.day) + getShowingIdFromGroup(showingGroup); 
+    }) 
+ 
+    return acc 
+  }, []).reduce((a, c, i) => { 
+    return a + `[${i}]` + c 
+  }, '') 
+  return `${startingDate}${dataString}`; 
+}
+
+function isMonthName(str: string): boolean {
+  return MONTHS.includes(str.toLowerCase());
+}
+
+export function fullCodeStringToReadable(str: string) {
+  const {startingDateString, showsDates} = urlCodeParts(str);
+  const startingDate = DateTime.fromISO(startingDateString);
+  let year = startingDate.toFormat('yyyy');
+
+  return showsDates.map(dateCodes => {
+    let output = year + '\n';
+    let runningMonth = startingDate.startOf('month');
+    
+    dateCodeStringToTokens(dateCodes).map((token) => {
+      if (token === '0') {
+        runningMonth = runningMonth.plus({months: 1});
+        return runningMonth.toFormat('LLLL');
+      }
+      const [dateId, slotId] = token;
+      return `${getDaysFromDateId(dateId) + 1}${getSlotShorthandFromSlotsId(slotId)}`;
+    }).reduce((acc: string[], token, i) => {
+      console.log({token})
+
+      if (i === 0 && isShowingCode(token)) {
+        console.log('fired month push');
+        
+        acc.push(startingDate.toFormat('LLLL'));
+      }
+      const lastToken = acc[i - 1];
+      if (isMonthName(token) && lastToken) {
+        if (isMonthName(lastToken)) {
+          acc[i - 1] = token;
+          return acc;
+        }
+      }
+      acc.push(token);
+      return acc;
+    }, []).forEach(token => {
+      console.log({finalToken: token})
+      if (isMonthName(token)) return output += `\n${token}`;
+      if (isShowingCode(token)) return output += ` ${token},`;
+      return output += token; // This should just be the year... I think
+    })
+    return output;
+  });
+}
+
+function dateCodeStringToTokens(str: string) {
+  return str.match(/(0|.{2})/g) || []
+}
+
+function getSlotShorthandFromSlotsId(slotsId: string) {
+  switch (slotsId) {
+    case '1': return 'm';
+    case '2': return 'a';
+    case '3': return 'e';
+    case '4': return 'ma';
+    case '5': return 'me';
+    case '6': return 'ae';
+    case '7': return 'mae';
+    default: throw new Error('unknown slotId of ' + slotsId);
+  }
+}
+
+function getShorthandObj(slotsId: string, showId: number): IDayShowings {
+  let props = getSlotShorthandFromSlotsId(slotsId);
+  const output: any = {};
+  Array.from(props).forEach(letter => output[letter] = showId);
+  return output;
+}
+
+function urlCodeParts(urlCode: string) {
+  const [startingDateString, ...showsDates] = urlCode.split(/\[\d*\]/g);
+  return {startingDateString, showsDates};
+}
+
+function urlToShorthandPerShow(urlCode: string) {
+  const {startingDateString, showsDates} =  urlCodeParts(urlCode);
+  return showsDates.map((c, showIndex) => {
+    const showId = showIndex + 1;
+    const dates: IDayShowings[] = [];
+    let startingDate = DateTime.fromISO(startingDateString);
+    let addedMonths = 0;
+    dateCodeStringToTokens(c).forEach((input) => {
+      if (input === '0') {
+        addedMonths++;
+        return;
+      }
+      const [dateId, slotsId] = input.split('');
+      const days = getDaysFromDateId(dateId);
+      const daysFromStart = startingDate.startOf('month').plus({months: addedMonths, days}).diff(startingDate, 'days').toObject().days || 0;
+      dates[daysFromStart] = getShorthandObj(slotsId, showId)
+    })
+    const a = [];
+    for (let index = 0; index < dates.length; index++) {
+      a[index] = Object.assign({}, dates[index] || {});
+    }
+    return a;
+  });
+}
+
+export function urlToShorthand(urlCode: string): IShorthandObject {
+  const {startingDateString: startingDate} =  urlCodeParts(urlCode);
+  const showData = urlToShorthandPerShow(urlCode).reduce((a, perfsForCurrentShow) => {
+    const longestLength = Math.max(a.length, perfsForCurrentShow.length);
+    for (let index = 0; index < longestLength; index++) {
+      a[index] = Object.assign(a[index] || {}, perfsForCurrentShow[index] || {});
+    }
+    return a
+  }, [])
+  return {startingDate, showData}
+}
+
+function getShowingIdFromGroup(group: TShowingCode) {
+  switch(group) {
+    case 'm': return 1;
+    case 'a': return 2;
+    case 'e': return 3;
+    case 'ma': return 4;
+    case 'me': return 5;
+    case 'ae': return 6;
+    case 'mae': return 7;
+    default: throw new Error('unrecognized grouping: ' + group);
+  }
+}
+
+
+function isShowingCode(str: string): boolean {
+  return MATCH_SHOWING_CODE.test(str)
+}
+
+function getUrlTokenFromReadableToken(token: string) {
+  const [day, showing] = (token.match(SPLIT_SHOWING_CODE) || []) as [string, TShowingCode];
+  return `${getDateIdFromDay(day)}${getShowingIdFromGroup(showing)}`;
+}
+
+function findEarliestStartDate(readables: string[]) {
+  let earliestStartingDate = DateTime.local().plus({years: 5});
+  readables.forEach((text) => {
+    const tokens = (text.match(/\s*(\d{4}|[A-z]+|\d{1,2}[mae]{1,3}),?\s*/g)||[]).map(t => t.replace(/,/g, '').trim())
+    let runningDate = DateTime.local().startOf('year');
+    let confirmedDate = false;
+    tokens.forEach((token) => {
+      if (confirmedDate) return;
+      if (/\d{1,2}[mae]{1,3}/.test(token)) {
+        confirmedDate = true;
+        runningDate = runningDate.set({day: parseInt(token)})
+        if (runningDate < earliestStartingDate) earliestStartingDate = runningDate;
+      } else if (/\d{4}/.test(token)) {
+        const newYear = DateTime.fromISO(`${token}-01-01`);
+        runningDate = newYear;
+      } else {
+        const month = MONTHS.indexOf(token.toLowerCase()) + 1
+        let newMonth = runningDate.set({month})
+        if (newMonth < runningDate) {
+          newMonth = newMonth.plus({years: 1});
+        }
+        runningDate = newMonth;
+      }
+    })
+  });
+  return earliestStartingDate;
+}
+
+function monthsDiffFromDays(first: DateTime, second: DateTime): number {
+  const yearsDiff = second.year - first.year;
+  const monthsDiff = second.month - first.month;
+  return yearsDiff * 12 + monthsDiff;
+}
+
+export function readablesToUrl(readables: string[]) {
+  let earliestStartingDate = findEarliestStartDate(readables)
+  const showsDates = readables.map((text, i) => {
+    let runningDate = earliestStartingDate;
+    let lastConfirmedDate: DateTime;
+    let output = `[${i+1}]`;
+    const tokens = (text.match(/\s*(\d{4}|[A-z]+|\d{1,2}[mae]{1,3}),?\s*/g) || []).map(t => t.replace(/,/g, '').trim())
+    tokens.forEach((token) => {
+      if (/\d{1,2}[mae]{1,3}/.test(token)) {
+        output += getUrlTokenFromReadableToken(token)
+        runningDate = runningDate.set({day: parseInt(token)})
+        lastConfirmedDate = runningDate; 
+      } else if (/\d{4}/.test(token)) {
+        const newYear = DateTime.fromISO(`${token}-01-01`);
+        runningDate = newYear;
+      } else {
+        const month = MONTHS.indexOf(token.toLowerCase()) + 1
+        let newMonth = runningDate.set({month})
+        if (newMonth < runningDate) {
+          newMonth = newMonth.plus({years: 1});
+        }
+        runningDate = newMonth;
+        if (lastConfirmedDate) {
+          const monthsDiff = monthsDiffFromDays(lastConfirmedDate, newMonth);
+          Array.from(new Array(monthsDiff)).forEach(() => output += '0')
+        }
+      }
+    })
+    return output;
+  });
+  return earliestStartingDate.toFormat('yyyy-MM-dd') + showsDates.join('');
+}
---
+++ tests/unit/utils/showings-data-converters-test.ts
@@ -0,0 +1,65 @@
+import * as dc from 'post-playhouse-calendar-renderer/utils/showings-data-converters';
+import { module, test } from 'qunit';
+
+const URL_CODE = '2018-06-29' +
+'[1]C20a3' +
+'[2]D30b2' +
+'[3]0f3';
+
+const READABLE = [
+`2018
+June 29a,
+July 1e`,
+`2018
+June 30e
+July 2a`,
+`2018
+July 6e`
+]
+
+const SHORTHAND = () => ({startingDate: '2018-06-29', showData: [
+  { "a": 1 },
+  { "e": 2 },
+  { "e": 1 },
+  { "a": 2 },
+  {},
+  {},
+  {},
+  { "e": 3 }
+]})
+
+function numDaysWithShowings(showings: dc.IDayShowings[]): number {
+  return showings.reduce((count, showing) => {
+    if (showing.a || showing.e || showing.m) return count + 1;
+    return count;
+  }, 0)
+}
+
+module('Unit | Utility | showings-data-converters | simple functions', function() {
+
+  test('getPaddingFor', function(assert) {
+    let result = dc.getPaddingFor('2018-06-01');
+    assert.equal(result, 5);
+  });
+
+  test('urlToShorthand', function(assert) {
+    let result = dc.urlToShorthand(URL_CODE);
+
+    assert.equal(numDaysWithShowings(result.showData), 5, 'has correct number of showings');
+    assert.equal(result.startingDate, '2018-06-29', 'has the correct starting date')
+    assert.deepEqual(result.showData, SHORTHAND().showData, 'show data looks correct');
+  });
+
+  test('shorthandToUrl', function(assert) {
+    let result = dc.shorthandToUrl(SHORTHAND());
+    assert.equal(result, URL_CODE)
+  });
+});
+
+module('Unit | Utility | showings-data-converters | simple functions', function() {
+
+  test('fullCodeStringToReadable', function(assert) {
+    let result = dc.fullCodeStringToReadable(URL_CODE);
+    assert.equal(result, READABLE)
+  });
+});
