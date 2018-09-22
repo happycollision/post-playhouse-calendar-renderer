4de73c33c111a145c0d3f7eff3112584b805420e
Don Denton <don@happycollision.com>
2018-09-22 18:28:07 -0500

    highlight days with possible errors

--- app/components/p-calendar/day.js
+++ app/components/p-calendar/day.js
@@ -3,7 +3,7 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
   classNames: ['day'],
-  classNameBindings: ['isDark:dark', 'whichDay'],
+  classNameBindings: ['isDark:dark', 'whichDay', 'isError:error'],
   whichDay: computed('mDay', function() { return `day-${this.mDay}`;}),
 
   displayMonthName: computed('monthName', function() {
@@ -18,5 +18,37 @@ export default Component.extend({
   isDark: computed('shows', function() {
     const length = this.get('shows.length');
     return (length === 0 || length === undefined)
+  }),
+
+  isError: computed('shows', 'dayNameFull', function() {
+    const {shows, dayNameFull} = this.getProperties('shows', 'dayNameFull')
+    if (shows.length === 0) return false;
+    
+    // Sunday time other than 2pm
+    if (dayNameFull === 'Sunday') {
+      if (shows.length > 1) return true;
+      if (shows[0].time !== '2p') return true;
+    }
+
+    // 10am is on a non Saturday
+    if (dayNameFull !== 'Saturday') {
+      if (shows.some(s => s.time === '10a')) return true;
+    }
+
+    // multiple showings
+    const counts = {m: 0, a: 0, e: 0};
+    shows.forEach(show => {
+      switch (show.time) {
+        case '10a': counts.m = counts.m + 1; return;
+        case '2p': counts.a = counts.a + 1; return;
+        case '8p': counts.e = counts.e + 1; return;
+      }
+    })
+    for (const prop in counts) {
+      if (counts[prop] > 1) return true;
+    }
+
+    // all clear!
+    return false;
   }),
 });
--- tests/integration/components/p-calendar/day-test.js
+++ tests/integration/components/p-calendar/day-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { ShowData } from 'post-playhouse-calendar-renderer/utils/showings-data-converters';
 
 module('Integration | Component | p calendar/day', function(hooks) {
   setupRenderingTest(hooks);
@@ -9,9 +10,52 @@ module('Integration | Component | p calendar/day', function(hooks) {
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.on('myAction', function(val) { ... });
+    const setData = (shows = [], day = 'Sunday', date = 1, month = 'Jun') => {
+      this.set('dayNameFull', day);
+      this.set('monthName', month);
+      this.set('mDay', date);
+      this.set('shows', shows);
+    }
+    setData();
 
-    await render(hbs`{{p-calendar/day}}`);
+    await render(hbs`{{p-calendar/day dayNameFull=dayNameFull mDay=mDay monthName=monthName shows=shows}}`);
 
     assert.ok(true);
+  });
+
+
+  test('it highlights days with possible issues', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+    const setData = (shows = [], day = 'Sunday', date = 1, month = 'Jun') => {
+      this.set('dayNameFull', day);
+      this.set('monthName', month);
+      this.set('mDay', date);
+      this.set('shows', shows);
+    }
+    setData();
+    await render(hbs`{{p-calendar/day dayNameFull=dayNameFull mDay=mDay monthName=monthName shows=shows}}`);
+
+    
+    setData([
+      new ShowData(1, '2p', {1: 'Some Show'}),
+      new ShowData(2, '8p', {1: 'Some Other Show'}),
+    ], 'Sunday')
+    assert.dom('.day').hasClass('error', 'error when sunday has a second show');
+
+    setData([new ShowData(1, '10a', {1: 'Some Show'})], 'Sunday')
+    assert.dom('.day').hasClass('error', 'error when sunday showtime is not 2pm');
+
+    setData([new ShowData(1, '2p', {1: 'Some Show'})], 'Sunday')
+    assert.dom('.day').hasNoClass('error', 'no error when sunday showtime is 2pm');
+
+    setData([new ShowData(1, '10a', {1: 'Some Show'})], 'Friday')
+    assert.dom('.day').hasClass('error', 'error when 10am is not Saturday');
+
+    setData([
+      new ShowData(1, '2p', {1: 'Some Show'}),
+      new ShowData(2, '2p', {1: 'Some Other Show'}),
+    ])
+    assert.dom('.day').hasClass('error', 'multiple showings at the same time');    
   });
 });
