a56f803e61ec569833330e35a905d6de8ce70fc5
Don Denton <don@happycollision.com>
2019-09-22 21:45:02 -0500

    only showingsData for edit-form

--- app/components/edit-form.ts
+++ app/components/edit-form.ts
@@ -1,7 +1,17 @@
 import Component from '@ember/component';
+import { computed } from '@ember-decorators/object';
+import {
+  ShowingsData,
+  fullCodeStringToReadable,
+} from 'post-playhouse-calendar-renderer/utils/showings-data-converters';
 
 export default class EditForm extends Component.extend({
   // anything which *must* be merged to prototype here
 }) {
+  showingsData!: ShowingsData;
   // normal class body definition here
+  @computed('showingsData')
+  get readableDates() {
+    return fullCodeStringToReadable(this.showingsData.datesUrl);
+  }
 }
